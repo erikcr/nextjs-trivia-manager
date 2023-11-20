6bca10c754b82d74823fab7471b2ef37510cd0cc
Erik C. Rutledge <4974496+erikcr@users.noreply.github.com>
2023-11-19 19:00:01 -0500

    Migrate header to use user details.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { createClient } from "@/utils/supabase/client";
 import Header from "@/components/Header";
 
 export default function Home() {
+  const supabase = createClient();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      console.log(data.user.id);
+    };
+
+    getUser();
+  });
+
   return (
     <>
       <Header />
