eae00722a3c326e1f98833588f3352cebd026828
Erik C. Rutledge <4974496+erikcr@users.noreply.github.com>
2023-11-22 16:30:40 -0500

    Support add event and error handling.

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -14,13 +14,6 @@ import { Tables } from "@/types/database.types";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import Notification from "@/components/Notification";
 
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
-];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Settings", href: "#" },
@@ -34,40 +27,72 @@ function classNames(...classes: any[]) {
 export default function DashboardPage() {
   const supabase = createClient();
 
+  // User
   const [user, setUser] = useState<User | null>(null);
-  const [open, setOpen] = useState(false);
-  const [show, setShow] = useState(false);
-  const [loading, setLoading] = useState(true);
+
+  // Notifications
+  const [notifShow, setNotifShow] = useState(false);
+  const [notifTitle, setNotifTitle] = useState("");
+  const [notifType, setNotifType] = useState("");
+
+  // Events
+  const [eLoading, setELoading] = useState(true);
+  const [eventSlideout, setEventSlideout] = useState(false);
+  const [addEventLoading, setAddEventLoading] = useState(false);
 
   const [allEvents, setAllEvents] = useState<
     Tables<"v001_events_stag">[] | null
   >([]);
 
-  const addEvent = async () => {
-    setShow(true);
-  };
-
-  const getAllEvents = async (userId: string | undefined) => {
+  const getAllEvents = async (userId?: string) => {
     const { data, error } = await supabase
       .from("v001_events_stag")
       .select()
-      .eq("owner", userId);
+      .eq("owner", userId ? userId : user?.id);
 
     if (data) {
       setAllEvents(data);
-      setLoading(false);
+      setELoading(false);
+    }
+  };
+
+  const addEvent = async (formData: FormData) => {
+    setAddEventLoading(true);
+    console.log("submit create");
+    const { data, error } = await supabase
+      .from("v001_events_stag")
+      .insert([
+        {
+          name: formData.get("event-name"),
+          date_of_event: formData.get("event-date"),
+          description: formData.get("event-description"),
+          location: formData.get("event-location"),
+          venue: formData.get("event-venue"),
+          owner: user?.id,
+        },
+      ])
+      .select();
+
+    if (!error) {
+      setNotifTitle("Event created");
+      setNotifType("success");
+      setNotifShow(true);
+      setEventSlideout(false);
+      getAllEvents();
+    } else {
+      console.log(error);
+    }
+  };
+
+  const getUser = async () => {
+    const { data } = await supabase.auth.getUser();
+    if (data) {
+      getAllEvents(data.user?.id);
+      setUser(data.user);
     }
   };
 
   useEffect(() => {
-    const getUser = async () => {
-      const { data } = await supabase.auth.getUser();
-      if (data) {
-        getAllEvents(data.user?.id);
-        setUser(data?.user);
-      }
-    };
-
     getUser();
   });
 
@@ -276,7 +301,7 @@ export default function DashboardPage() {
                   type="button"
                   className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                   onClick={() => {
-                    setOpen(true);
+                    setEventSlideout(true);
                   }}
                 >
                   <svg
@@ -299,7 +324,7 @@ export default function DashboardPage() {
                 </button>
               </li>
 
-              {loading && (
+              {eLoading && (
                 <li className="animate-pulse overflow-hidden rounded-xl border border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                   <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 dark:bg-gray-600 p-6">
                     <div className="text-sm font-medium leading-6 text-gray-400 dark:text-gray-200">
@@ -329,7 +354,7 @@ export default function DashboardPage() {
                 </li>
               )}
 
-              {!loading &&
+              {!eLoading &&
                 allEvents?.map((item) => (
                   <Link key={item.id} href={`/dashboard/event/${item.id}`}>
                     <li
@@ -379,8 +404,8 @@ export default function DashboardPage() {
       {/**
        * New event panel
        */}
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+      <Transition.Root show={eventSlideout} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setEventSlideout}>
           <Transition.Child
             as={Fragment}
             enter="ease-in-out duration-500"
@@ -408,7 +433,7 @@ export default function DashboardPage() {
                   <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
                     <form
                       className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl"
-                      onSubmit={addEvent}
+                      action={addEvent}
                     >
                       <div className="flex-1">
                         {/* Header */}
@@ -427,7 +452,7 @@ export default function DashboardPage() {
                               <button
                                 type="button"
                                 className="relative text-gray-400 hover:text-gray-500"
-                                onClick={() => setOpen(false)}
+                                onClick={() => setEventSlideout(false)}
                               >
                                 <span className="absolute -inset-2.5" />
                                 <span className="sr-only">Close panel</span>
@@ -441,7 +466,7 @@ export default function DashboardPage() {
                         </div>
 
                         {/* Divider container */}
-                        <div className="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+                        <div className="space-y-6 py-6 sm:space-y-0 sm:border-b sm:border-1 sm:py-0">
                           {/* Event name */}
                           <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
                             <div>
@@ -449,38 +474,65 @@ export default function DashboardPage() {
                                 htmlFor="event-name"
                                 className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5"
                               >
-                                Name
+                                Name <span className="text-red-600">*</span>
                               </label>
                             </div>
                             <div className="sm:col-span-2">
                               <input
+                                required
+                                disabled={addEventLoading}
                                 type="text"
                                 name="event-name"
                                 id="event-name"
+                                placeholder="The Next Great Trivia"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                               />
                             </div>
                           </div>
 
-                          {/* Event description */}
+                          {/* Date of event */}
                           <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
                             <div>
                               <label
-                                htmlFor="event-description"
+                                htmlFor="event-date"
                                 className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5"
                               >
-                                Description
+                                Date of event{" "}
+                                <span className="text-red-600">*</span>
                               </label>
                             </div>
                             <div className="sm:col-span-2">
-                              <textarea
-                                id="event-description"
-                                name="event-description"
-                                rows={3}
+                              <input
+                                required
+                                disabled={addEventLoading}
+                                type="date"
+                                name="event-date"
+                                id="event-date"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                defaultValue={""}
                               />
                             </div>
+                          </div>
+                        </div>
+
+                        {/* Event description */}
+                        <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                          <div>
+                            <label
+                              htmlFor="event-description"
+                              className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5"
+                            >
+                              Description
+                            </label>
+                          </div>
+                          <div className="sm:col-span-2">
+                            <textarea
+                              disabled={addEventLoading}
+                              id="event-description"
+                              name="event-description"
+                              rows={3}
+                              placeholder="An optional description of The Next Great Trivia event with relevant details for the players."
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
                           </div>
                         </div>
 
@@ -496,9 +548,12 @@ export default function DashboardPage() {
                           </div>
                           <div className="sm:col-span-2">
                             <input
+                              disabled={addEventLoading}
                               type="text"
                               name="event-location"
                               id="event-location"
+                              autoComplete="home city"
+                              placeholder="Chattanooga, TN"
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                           </div>
@@ -516,9 +571,11 @@ export default function DashboardPage() {
                           </div>
                           <div className="sm:col-span-2">
                             <input
+                              disabled={addEventLoading}
                               type="text"
                               name="event-venue"
                               id="event-venue"
+                              placeholder="South Side Social"
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                           </div>
@@ -529,17 +586,44 @@ export default function DashboardPage() {
                       <div className="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
                         <div className="flex justify-end space-x-3">
                           <button
+                            disabled={addEventLoading}
                             type="button"
                             className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                            onClick={() => setOpen(false)}
+                            onClick={() => setEventSlideout(false)}
                           >
                             Cancel
                           </button>
                           <button
+                            disabled={addEventLoading}
                             type="submit"
                             className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                           >
-                            Create
+                            {addEventLoading ? (
+                              <>
+                                <div role="status" className="pr-2">
+                                  <svg
+                                    aria-hidden="true"
+                                    className="w-5 h-5 text-gray-500 animate-spin dark:text-gray-600 fill-white"
+                                    viewBox="0 0 100 101"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                      fill="currentColor"
+                                    />
+                                    <path
+                                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                      fill="currentFill"
+                                    />
+                                  </svg>
+                                  <span className="sr-only">Loading...</span>
+                                </div>
+                                Loading...
+                              </>
+                            ) : (
+                              <>Create</>
+                            )}
                           </button>
                         </div>
                       </div>
@@ -553,13 +637,13 @@ export default function DashboardPage() {
       </Transition.Root>
 
       {/**
-       * Event creation success notification
+       * Action notification
        */}
       <Notification
-        title="Event created"
-        type="success"
-        show={show}
-        setShow={setShow}
+        title={notifTitle}
+        type={notifType}
+        show={notifShow}
+        setShow={setNotifShow}
       />
     </div>
   );
