3a0243aef1b0f8714c008762e5a4a4fbe0f3ec77
Erik C. Rutledge <4974496+erikcr@users.noreply.github.com>
2023-12-10 13:24:34 -0600

    Show notif modal when failed login.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,8 +4,15 @@ import { redirect } from "next/navigation";
 import Image from "next/image";
 
 import logoBrainyBrawls from "@/public/logos/brainybrawls.svg";
+import { XCircleIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
-export default function LoginScreen({}: {}) {
+export default function LoginScreen({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   const signIn = async (formData: FormData) => {
     "use server";
 
@@ -134,6 +141,37 @@ export default function LoginScreen({}: {}) {
           </p>
         </div>
       </div>
+
+      {searchParams?.message && (
+        <div
+          aria-live="assertive"
+          className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+        >
+          <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+            {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <XCircleIcon
+                      className="h-6 w-6 text-red-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">
+                      Login failed
+                    </p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {searchParams.message}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
