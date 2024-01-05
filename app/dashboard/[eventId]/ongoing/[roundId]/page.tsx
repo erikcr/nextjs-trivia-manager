2c3afc4ee1dfb5cfbf53477c1cc604d9eb5cf929
Erik C. Rutledge <4974496+erikcr@users.noreply.github.com>
2024-01-05 09:50:36 -0500

    Change color to app primary color.

--- app/dashboard/[eventId]/ongoing/[roundId]/page.tsx
+++ app/dashboard/[eventId]/ongoing/[roundId]/page.tsx
@@ -350,7 +350,7 @@ export default function EventOngoingPage() {
               href={`/dashboard/${eventId}/ongoing/${item.id}`}
               className={classNames(
                 item.id === Number(roundId)
-                  ? "bg-indigo-100 text-indigo-700"
+                  ? "bg-primary text-white"
                   : "text-gray-500 hover:text-gray-700",
                 "rounded-md px-3 py-2 text-sm font-medium"
               )}
