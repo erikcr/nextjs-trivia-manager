e6c3b42bb095a69275d046599d58456dd1ba24eb
Erik C. Rutledge <4974496+erikcr@users.noreply.github.com>
2024-12-03 17:36:44 -0500

    minor package upgrades

--- app/dashboard/[eventId]/editor/page.tsx
+++ app/dashboard/[eventId]/editor/page.tsx
@@ -3,12 +3,13 @@
 import { FormEvent, Fragment, useEffect, useRef, useState } from "react";
 import { useParams, useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
-import { Dialog, Menu, Transition } from "@headlessui/react";
+import { Dialog, Listbox, Menu, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
   RocketLaunchIcon,
   CheckIcon,
   PencilSquareIcon,
+  ChevronDownIcon,
 } from "@heroicons/react/24/outline";
 import { useChat } from "ai/react";
 
@@ -246,6 +247,7 @@ export default function EditorByIdPage() {
   };
 
   const removeQuestionFromAI = async () => {
+    if (!aiResponse) return;
     const newAiResponse = aiResponse.slice(1);
     setAiResponse(newAiResponse);
     setQuestionToAdd(newAiResponse[0]);
@@ -325,7 +327,7 @@ export default function EditorByIdPage() {
       {/**
        * Top header
        */}
-      <div className="fixed top-0 left-0 right-0 flex h-16 shrink-0 items-center gap-x-4 border-b bg-primary dark:bg-primary-dark border-gray-400 dark:border-zinc-700 text-gray-900 dark:text-gray-200">
+      <div className="fixed top-0 left-0 right-0 z-50 flex h-16 shrink-0 items-center gap-x-4 border-b bg-primary dark:bg-primary-dark border-gray-400 dark:border-zinc-700 text-gray-900 dark:text-gray-200">
         <TopHeader />
       </div>
 
@@ -466,6 +468,21 @@ export default function EditorByIdPage() {
   );
 
   function TopHeader() {
+    const publishingOptions = [
+      {
+        title: "Editing",
+        description:
+          "This job posting can be viewed by anyone who has the link.",
+        current: true,
+      },
+      {
+        title: "Ongoing",
+        description: "This job posting will no longer be publicly accessible.",
+        current: false,
+      },
+    ];
+    const [selected, setSelected] = useState(publishingOptions[0]);
+
     return (
       <div className="w-full">
         <nav
@@ -506,16 +523,102 @@ export default function EditorByIdPage() {
 
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             {event !== undefined && (
-              <span
-                className={classNames(
-                  event?.status === "PENDING"
-                    ? "bg-blue-100 dark:bg-blue-900"
-                    : "bg-green-100 dark:bg-green-900",
-                  "inline-flex items-center rounded-full px-2 mr-3 py-1 text-sm  text-gray-600 dark:text-gray-400 ring-1 ring-inset"
+              <Listbox value={selected} onChange={setSelected}>
+                {({ open }) => (
+                  <>
+                    <Listbox.Label className="sr-only">
+                      Change published status
+                    </Listbox.Label>
+                    <div className="relative">
+                      <div className="inline-flex divide-x divide-indigo-700 rounded-md shadow-sm">
+                        <div className="inline-flex items-center gap-x-1.5 rounded-l-md bg-indigo-600 px-3 py-2 text-white shadow-sm">
+                          <CheckIcon
+                            className="-ml-0.5 h-5 w-5"
+                            aria-hidden="true"
+                          />
+                          <p className="text-sm font-semibold">
+                            {selected.title}
+                          </p>
+                        </div>
+                        <Listbox.Button className="inline-flex items-center rounded-l-none rounded-r-md bg-indigo-600 p-2 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-offset-2 focus:ring-indigo-600 focus:ring-offset-gray-50">
+                          <span className="sr-only">
+                            Change published status
+                          </span>
+                          <ChevronDownIcon
+                            className="h-5 w-5 text-white"
+                            aria-hidden="true"
+                          />
+                        </Listbox.Button>
+                      </div>
+
+                      <Transition
+                        show={open}
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Listbox.Options className="absolute right-0 z-50 mt-2 w-72 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {publishingOptions.map((option) => (
+                            <Listbox.Option
+                              key={option.title}
+                              className={({ active }) =>
+                                classNames(
+                                  active
+                                    ? "bg-indigo-600 text-white"
+                                    : "text-gray-900",
+                                  "cursor-default select-none p-4 text-sm"
+                                )
+                              }
+                              value={option}
+                            >
+                              {({ selected, active }) => (
+                                <div className="flex flex-col">
+                                  <div className="flex justify-between">
+                                    <p
+                                      className={
+                                        selected
+                                          ? "font-semibold"
+                                          : "font-normal"
+                                      }
+                                    >
+                                      {option.title}
+                                    </p>
+                                    {selected ? (
+                                      <span
+                                        className={
+                                          active
+                                            ? "text-white"
+                                            : "text-indigo-600"
+                                        }
+                                      >
+                                        <CheckIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </span>
+                                    ) : null}
+                                  </div>
+                                  <p
+                                    className={classNames(
+                                      active
+                                        ? "text-indigo-200"
+                                        : "text-gray-500",
+                                      "mt-2"
+                                    )}
+                                  >
+                                    {option.description}
+                                  </p>
+                                </div>
+                              )}
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Transition>
+                    </div>
+                  </>
                 )}
-              >
-                {event?.status}
-              </span>
+              </Listbox>
             )}
 
             <ThemeSwitcher />
@@ -524,7 +627,7 @@ export default function EditorByIdPage() {
               <button
                 type="button"
                 disabled={startDisabled}
-                className="inline-flex items-center gap-x-1.5 rounded-md px-2.5 py-1.5 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                className="inline-flex items-center gap-x-1.5 rounded-md px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
                 onClick={() => {
                   setStartConfirmShow(true);
                 }}
@@ -1007,7 +1110,7 @@ export default function EditorByIdPage() {
                   id="question"
                   rows={2}
                   placeholder="Question"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 dark:bg-zinc-800 dark:text-gray-200 shadow-sm dark:shadow-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary dark:focus-primary-dark sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 dark:bg-zinc-800 dark:text-gray-200 shadow-sm dark:shadow-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-md sm:leading-6"
                   defaultValue={questionToEdit ? questionToEdit.question : ""}
                 />
               </div>
